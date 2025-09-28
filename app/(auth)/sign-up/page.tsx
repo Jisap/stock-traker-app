eca9853de0a1879ec08dfc8cf7733395821daed0
Jorge Matos <jisapjisap@gmail.com>
2025-09-28 11:29:38 +0200

    Update package dependencies and implement sign-up form with validation; add custom Label and Select components for improved user input handling.

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,48 @@
+"use client"
+
 import React from 'react'
+import { useForm } from 'react-hook-form';
+
+
+
 
 const signUp = () => {
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: '',
+      email: '',
+      password: '',
+      country: 'US',
+      investmentGoals: 'Growth',
+      riskTolerance: 'Medium',
+      preferredIndustry: 'Technology'
+    },
+    mode: 'onBlur'
+  },);
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>signUp</div>
+    <>
+      <h1 className="form-title">Sign Up & Personalize</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        
+      </form>
+
+    </>
   )
 }
 
