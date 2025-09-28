179ef6c6974d1597d44bb78f2b6c3ccf23cc1f55
Jorge Matos <jisapjisap@gmail.com>
2025-09-28 11:35:36 +0200

    Add sign-up form enhancements: integrate custom InputField and FooterLink components, and implement a submit button with loading state for improved user experience.

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import FooterLink from '@/components/forms/FooterLink';
+import { Button } from '@/components/ui/button';
 import React from 'react'
 import { useForm } from 'react-hook-form';
 
@@ -38,9 +40,27 @@ const signUp = () => {
     <>
       <h1 className="form-title">Sign Up & Personalize</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
-        
+      <form 
+        onSubmit={handleSubmit(onSubmit)} 
+        className="space-y-5"
+      >
+        {/* Inputs */}
+
       </form>
+
+      <Button 
+        type="submit" 
+        disabled={isSubmitting} 
+        className="yellow-btn w-full mt-5"
+      >
+        {isSubmitting ? 'Creating Account' : 'Start Your Investing Journey'}
+      </Button>
+
+      <FooterLink 
+        text="Already have an account?" 
+        linkText="Sign in" 
+        href="/sign-in" 
+      />
 
     </>
   )
